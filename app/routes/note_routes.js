5c56978b45d37a05db8ed7435a615c88f56f6291
amuhammad135 <am4411@columbia.edu>
2017-10-11 02:33:43 -0400

    set up initial database, need to change some of the naming

---
+++ app/routes/note_routes.js
@@ -0,0 +1,55 @@
+var ObjectID = require('mongodb').ObjectID;	
+
+module.exports = function(app, db) {
+	app.get('/notes/:id', (req, res) => {
+		const id = req.params.id;
+		const details = { '_id': new ObjectID(id) };
+		db.collection('notes').findOne(details, (err, item) =>  {
+			if (err) {
+				res.send({'error' : 'An error has occurred'});
+			}
+			else {
+				res.send(item);
+			}
+		});
+	});
+
+	app.delete('notes/:id', (req, res) => {
+		const id = req.params.id;
+		const details = { '_id' : new ObjectID(id) };
+		db.collection('notes').remove(details, (err, item) =>{
+			if (err) {
+				res.send({'error':'An error has occured'});
+			}
+			else {
+				res.send('Note ' + id + ' deleted!');
+			}
+		});
+	});
+
+	app.put('/notes/:id', (req, res) => {
+	    const id = req.params.id;
+	    const details = { '_id': new ObjectID(id) };
+	    const note = { username: req.body.username, password: req.body.password };
+	    db.collection('notes').update(details, note, (err, result) => {
+	      if (err) {
+	          res.send({'error':'An error has occurred'});
+	      } else {
+	          res.send(note);
+	      } 
+	    });
+	  });
+
+	app.post('/notes', (req, res) => {
+		// Create note
+		const note = {username: req.body.username, password: req.body.password};
+		db.collection('notes').insert(note, (err, result) => {
+			if (err) {
+				res.send({ 'error': 'An error has occurred' });
+			}
+			else {
+				res.send(result.ops[0]);
+			}
+		});
+	});
+};
--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const express = require('express');
+const express 		= require('express');
+const MongoClient 	= require('mongodb').MongoClient;
+const bodyParser 	= require('body-parser');
+const db 			= require('./config/db');
+
+
 const app = express();
 
+/*
 app.get('/', (req, res) => {
 	res.send('Hello world');
 })
+*/
 
-app.listen(3000, () => {
-	console.log('App is listening on port 3000');
-})
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+MongoClient.connect(db.url, (err, database) => {
+	if (err) return console.log(err)
+	require('./app/routes') (app, database);
+
+	app.listen(port, () => {
+		console.log('App is listening on port ' + port);
+	});
+});
